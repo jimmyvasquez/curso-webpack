f3499c57597ca899ecd26cfdbe0b47bf9e20953e
Jimmy Vasquez <jimmy.vasquez22@gmail.com>
2021-05-26 15:27:07 -0500

    server dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dovenv = require('dotenv-webpack');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 module.exports = {
     entry: './src/index.js',
@@ -12,6 +13,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
+    devtool: 'source-map',
     // watch: true,
     resolve: {
         extensions: ['.js'],
@@ -76,5 +78,13 @@ module.exports = {
             ]
         }),
         new Dovenv(),
-    ]
+        new BundleAnalyzerPlugin(),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+        open: true
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const Dovenv = require('dotenv-webpack');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
+
 module.exports = {
     entry: './src/index.js',
     output: {
